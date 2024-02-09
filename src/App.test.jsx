2f6b4cb8e0b49703166b627836679a5fc0618059
Brenda  Mukindia <mukindiabrenda67@gmail.com>
2024-02-09 14:10:34 +0300

    move files to one file

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,6 +4,13 @@ import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
+  test("renders the initial todo list correctly", async () => {
+    const { getByText, getByPlaceholderText } = render(<App />);
+    expect(getByText("My Todos")).not.toBeNull();
+
+    expect(getByPlaceholderText("What's the title of your To Do?")).not.toBeNull();
+    expect(getByPlaceholderText("What's the description of your To Do?")).not.toBeNull();
+  });
   test("adds a todo when the form is submitted", () => {
     const { getByPlaceholderText, getByText, getAllByTestId } = render(<App />);
 
@@ -21,4 +28,6 @@ describe("App", () => {
     expect(getAllByTestId("TodoItemTitle")).toHaveLength(1);
     expect(getAllByTestId("TodoItemTitle")[0].textContent).toBe("Test Todo");
   });
+
+
 });
